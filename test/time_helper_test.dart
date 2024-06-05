921bb112ceabd929edf885563d5c95be34d9b11e
sy4kila <norsyakila9@gmail.com>
2024-06-06 00:21:08 +0800

    Factory Management App

---
+++ test/time_helper_test.dart
@@ -0,0 +1,23 @@
+// test/time_helper_test.dart
+import 'package:factory_mgmt2/util/time_helper.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+void main() {
+  group("TimeHelper", () {
+    test("should return Night", () {
+      expect(TimeHelper.getTimeOfTheDay(DateTime(2023, 1, 1, 5)), "Night");
+    });
+
+    test("should return Morning", () {
+      expect(TimeHelper.getTimeOfTheDay(DateTime(2023, 1, 1, 9)), "Morning");
+    });
+
+    test("should return Afternoon", () {
+      expect(TimeHelper.getTimeOfTheDay(DateTime(2023, 1, 1, 17)), "Afternoon");
+    });
+
+    test("should return Evening", () {
+      expect(TimeHelper.getTimeOfTheDay(DateTime(2023, 1, 1, 22)), "Evening");
+    });
+  });
+}
