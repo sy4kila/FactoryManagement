533c556314e217294c4f8dc7ee43adf0ae1c2105
sy4kila <norsyakila9@gmail.com>
2024-06-07 01:41:10 +0800

    Factory Management App

---
+++ android/app/src/androidTest/java/com/example/factory_mgmt2/ExampleInstrumentedTest.kt
@@ -0,0 +1,19 @@
+package com.example.factory_mgmt2
+
+import android.content.Context
+import androidx.test.platform.app.InstrumentationRegistry
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import org.junit.Test
+import org.junit.runner.RunWith
+
+import org.junit.Assert.*
+
+@RunWith(AndroidJUnit4::class)
+class ExampleInstrumentedTest {
+    @Test
+    fun useAppContext() {
+        // Context of the app under test.
+        val appContext: Context = InstrumentationRegistry.getInstrumentation().targetContext
+        assertEquals("com.example.factory_mgmt2", appContext.packageName)
+    }
+}
